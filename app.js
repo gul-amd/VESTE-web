727831c5febc4cc698ce3da7436498d1dd119f06
Gulamo <gulamo.amd@gmail.com>
2024-11-25 19:59:29 +0200

    chore: ultimos acertos

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', () => {
+    alert('Você clicou em um serviço!');
+  });
+});
